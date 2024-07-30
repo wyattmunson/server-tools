203eecbe8b5f5ca3ea0bcdadadc7a9bd8309cc45
Wyatt <munsonwf@gmail.com>
2024-07-30 05:58:05 +0000

    add: s3 sync

--- s3sync.py
+++ s3sync.py
@@ -8,22 +8,22 @@ def load_credentials(credentials_file):
     credentials = json.load(f)
   return credentials
 
-def sync_directory_to_s3(local_dir, bucket_name, prefix=''):
-  """Syncs a local directory to an S3 bucket.
+def sync_directory_to_s3(local_dir, bucket_name, prefix='', credentials_file='credentials.json'):
+  """Syncs a local directory to an S3 bucket using credentials from a file.
 
   Args:
     local_dir: The local directory to sync.
     bucket_name: The name of the S3 bucket.
     prefix: The optional prefix for the S3 objects.
+    credentials_file: The path to the credentials file.
   """
-  credentials_file = "/home/mainframe/.creds/aws.json"
+
   credentials = load_credentials(credentials_file)
   session = boto3.Session(
       aws_access_key_id=credentials['aws_access_key_id'],
       aws_secret_access_key=credentials['aws_secret_access_key'],
       region_name=credentials.get('region_name', 'us-east-1')
   )
-
   s3 = session.client('s3')
 
   for root, dirs, files in os.walk(local_dir):
@@ -33,14 +33,16 @@ def sync_directory_to_s3(local_dir, bucket_name, prefix=''):
       s3_path = os.path.join(prefix, relative_path)
 
       try:
-        s3.upload_file(local_path, bucket_name, s3_path)
-        print(f"Uploaded {local_path} to s3://{bucket_name}/{s3_path}")
+        with open(local_path, 'rb') as data:
+          s3.put_object(Body=data, Bucket=bucket_name, Key=s3_path)
+          print(f"Uploaded {local_path} to s3://{bucket_name}/{s3_path}")
       except Exception as e:
         print(f"Error uploading {local_path}: {e}")
 
 if __name__ == "__main__":
-  local_dir = '/home/mainframe/IOTstack'
+  local_dir = '/path/to/your/local/directory'
   bucket_name = 'wycloud-backup'
-  prefix = 'theflame/IOTstack'  # Optional
+  prefix = 'theflame/'  # Optional
+  credentials_file = '/home/mainframe/.creds/aws.json'
 
-  sync_directory_to_s3(local_dir, bucket_name, prefix)
+  sync_directory_to_s3(local_dir, bucket_name, prefix, credentials_file)
